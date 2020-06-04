6f8c5db928107ae5bd7a12387cd7914152660d95
Rawgher <rdpender3@gmail.com>
2020-06-04 12:21:29 -0700

    creating recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,61 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+  # before_action :require_user, except: [:show, :index]
+  # before_action :require_same_user, only: [:edit, :update, :destroy]
+
+  def show
+  end
+
+  def index
+    @recipes = Recipe.paginate(page: params[:page], per_page: 3)
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    # @recipe.user = current_user
+    if @recipe.save
+      flash[:notice] = "Recipe was added successfully"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      flash[:notice] = "Updated recipe successfully"
+      redirect_to @recipe
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+
+  def set_recipe
+    @article = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description, :image, category_ids: [])
+  end
+  #
+  # def require_same_user
+  #   if current_user != @recipe.user && !current_user.admin?
+  #     flash[:alert] = "You can only edit or delete your own article"
+  #     redirect_to @recipe
+  #   end
+  # end
 end
