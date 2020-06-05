ee151beb4fa04b8fd4a33f948ea92339b9aa1a54
Rawgher <rdpender3@gmail.com>
2020-06-05 12:37:27 -0700

    adding route, delete to controller, and skeleton pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
   # before_action :require_admin, except: [:index, :show]
 
   def new
@@ -16,11 +17,9 @@ class CategoriesController < ApplicationController
   end
 
   def edit
-    @category = Category.find(params[:id])
   end
 
   def update
-    @category = Category.find(params[:id])
     if @category.update(category_params)
       # flash[:notice] = "Category name was updated"
       redirect_to category_path(@category)
@@ -34,11 +33,19 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
+  def destroy
+    @category.destroy
+    redirect_to categories_path
+  end
+
   private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name)
