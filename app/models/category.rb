877c8b23d751d96f54826b4de9f160b50318890b
Rawgher <rdpender3@gmail.com>
2020-06-09 17:13:40 -0700

    adding in recipes_category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
-  # has_many :articles_categories
-  # has_many :articles, through: :articles_categories
+  has_many :recipes_categories
+  has_many :recipes, through: :recipes_categories
 end
