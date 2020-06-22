886e0cd1a78b52d5445eed210e6d51fd0c269193
Rawgher <rdpender3@gmail.com>
2020-06-22 16:18:21 -0700

    fixing login form and some small changes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -29,7 +29,7 @@ class CategoriesController < ApplicationController
   end
 
   def index
-    @categories = Category.paginate(page: params[:page], per_page: 8)
+    @categories = Category.paginate(page: params[:page], per_page: 12)
   end
 
   def show
